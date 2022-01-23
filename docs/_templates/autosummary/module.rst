21f131be62a197dd6282865e86b2259cb73fdf6c
tfrederiksen <tfrederiksen@users.noreply.github.com>
2022-01-23 10:54:47 +0000

    Deploying to gh-pages from @ tfrederiksen/sisl@a8efd77fde4f94d2872bee8745a6592fe1a6dcf0 🚀

--- docs/_templates/autosummary/module.rst
+++
@@ -1,4 +0,0 @@
-.. automodule:: {{ fullname }}
-   :no-members:
-   :no-undoc-members:
-   :no-inherited-members:
--- sisl/io/_help.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['starts_with_list']
-
-
-def starts_with_list(l, comments):
-    for comment in comments:
-        if l.strip().startswith(comment):
-            return True
-    return False
