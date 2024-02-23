ceed3037d30031fb49bac8423fe0e823488b5fe2
tfrederiksen <tfrederiksen@users.noreply.github.com>
2024-02-23 10:52:10 +0000

    Deploying to gh-pages from @ tfrederiksen/sisl@73e3f9d039df9adefebe062d7cbc7b62366c2fee 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g851d78c',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1-73e3f9d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
